b335ddda7e0c40277b5edae68a631a5ab040f0b1
HaveMC_XiaoCai <lxx601588@outlook.com>
2025-08-07 17:15:48 +0800

    My first useful mod

---
+++ src/main/java/com/fireboy637/translatabledebugoptions/mixin/DebugOptionScreenMixin.java
@@ -0,0 +1,16 @@
+package com.fireboy637.translatabledebugoptions.mixin;
+
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
+import net.minecraft.text.Text;
+import net.minecraft.util.Identifier;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+
+@Mixin(targets = "net/minecraft/client/gui/screen/DebugOptionsScreen$Entry")
+public class DebugOptionScreenMixin {
+    @WrapOperation(method = "<init>", at = @At(value = "INVOKE", target = "Lnet/minecraft/util/Identifier;getPath()Ljava/lang/String;"))
+    private String pathToKey(Identifier instance, Operation<String> original) {
+        return Text.translatable("text.debug." + instance.getPath()).getString();
+    }
+}
