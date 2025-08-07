273552cf7fa643ad35ca991b1b2709c5dbdd1eaf
HaveMC_XiaoCai <lxx601588@outlook.com>
2025-08-07 21:33:57 +0800

    fix search and bump version

--- src/main/java/com/fireboy637/translatabledebugoptions/mixin/DebugOptionScreenEntryMixin.java
+++ src/main/java/com/fireboy637/translatabledebugoptions/mixin/DebugOptionScreenEntryMixin.java
@@ -8,7 +8,7 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 
 @Mixin(targets = "net/minecraft/client/gui/screen/DebugOptionsScreen$Entry")
-public class DebugOptionScreenMixin {
+public abstract class DebugOptionScreenEntryMixin {
     @WrapOperation(method = "<init>", at = @At(value = "INVOKE", target = "Lnet/minecraft/util/Identifier;getPath()Ljava/lang/String;"))
     private String pathToKey(Identifier instance, Operation<String> original) {
         return Text.translatable("debug." + instance.getNamespace() + "." + instance.getPath()).getString();
