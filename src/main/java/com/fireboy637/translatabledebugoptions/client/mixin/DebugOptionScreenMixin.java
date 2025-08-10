ddebc71889118a25470f82633ff5684d6c338e0e
HaveMC_XiaoCai <lxx601588@outlook.com>
2025-08-10 14:45:42 +0800

    Add Fabric API dependency and remake search handling

--- src/main/java/com/fireboy637/translatabledebugoptions/client/mixin/DebugOptionScreenMixin.java
+++ src/main/java/com/fireboy637/translatabledebugoptions/client/mixin/DebugOptionScreenMixin.java
@@ -2,27 +2,34 @@ package com.fireboy637.translatabledebugoptions.client.mixin;
 
 import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
 import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
+import com.llamalad7.mixinextras.sugar.Local;
+import net.minecraft.client.gui.hud.debug.DebugHudEntry;
 import net.minecraft.text.Text;
 import net.minecraft.util.Identifier;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 
+import java.util.Map;
+
 public abstract class DebugOptionScreenMixin {
     // Make getPath() return translated string. That's all.
-    // But for some reason, translation file can't be applied in dev environment.
+    // Though no code here uses Fabric API, but we still need to install it, or we can only see the translation keys.
     @Mixin(targets = "net/minecraft/client/gui/screen/DebugOptionsScreen$Entry")
     public static abstract class DebugOptionScreenEntryMixin {
         @WrapOperation(method = "<init>", at = @At(value = "INVOKE", target = "Lnet/minecraft/util/Identifier;getPath()Ljava/lang/String;"))
-        private String pathToKey(Identifier identifier, Operation<String> original) {
+        private String entryLabelHandler(Identifier identifier, Operation<String> original) {
             return Text.translatable("debug." + identifier.getNamespace() + "." + identifier.getPath()).getString();
         }
     }
 
     @Mixin(targets = "net/minecraft/client/gui/screen/DebugOptionsScreen$OptionsListWidget")
     public static abstract class DebugOptionScreenOptionsListWidgetMixin {
-        @WrapOperation(method = "fillEntries", at = @At(value = "INVOKE", target = "Lnet/minecraft/util/Identifier;getPath()Ljava/lang/String;"))
-        private String searchPathToKey(Identifier identifier, Operation<String> operation) {
-            return Text.translatable("debug." + identifier.getNamespace() + "." + identifier.getPath()).getString();
+        @WrapOperation(method = "fillEntries", at = @At(value = "INVOKE", target = "Ljava/lang/String;contains(Ljava/lang/CharSequence;)Z"))
+        private boolean searchHandler(String string, CharSequence searchCharSequence, Operation<Boolean> original, @Local Map.Entry<Identifier, DebugHudEntry> entry) {
+            Identifier identifier = entry.getKey();
+            String searchStringLower = searchCharSequence.toString().toLowerCase();
+            return original.call(string.toLowerCase(), searchStringLower) ||
+                    Text.translatable("debug." + identifier.getNamespace() + "." + identifier.getPath()).getString().toLowerCase().contains(searchStringLower);
         }
     }
 }
