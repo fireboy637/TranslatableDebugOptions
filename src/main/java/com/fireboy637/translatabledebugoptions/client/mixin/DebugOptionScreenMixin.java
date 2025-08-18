b49ba33663ba7465605a587700d88e421a10b0ee
HaveMC_XiaoCai <lxx601588@outlook.com>
2025-08-18 19:17:37 +0800

    something #4

--- src/main/java/com/fireboy637/translatabledebugoptions/client/mixin/DebugOptionScreenMixin.java
+++ src/main/java/com/fireboy637/translatabledebugoptions/client/mixin/DebugOptionScreenMixin.java
@@ -3,7 +3,6 @@ package com.fireboy637.translatabledebugoptions.client.mixin;
 import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
 import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
 import com.llamalad7.mixinextras.sugar.Local;
-import net.minecraft.client.gui.hud.debug.DebugHudEntry;
 import net.minecraft.text.Text;
 import net.minecraft.util.Identifier;
 import org.spongepowered.asm.mixin.Mixin;
@@ -11,11 +10,11 @@ import org.spongepowered.asm.mixin.injection.At;
 
 import java.util.Map;
 
-public abstract class DebugOptionScreenMixin {
+public interface DebugOptionScreenMixin {
     // Make getPath() return translated string. That's all.
     // Though no code here uses Fabric API, but we still need to install it, or we can only see the translation keys.
     @Mixin(targets = "net/minecraft/client/gui/screen/DebugOptionsScreen$Entry")
-    public static abstract class DebugOptionScreenEntryMixin {
+    abstract class DebugOptionScreenEntryMixin {
         @WrapOperation(method = "<init>", at = @At(value = "INVOKE", target = "Lnet/minecraft/util/Identifier;getPath()Ljava/lang/String;"))
         private String entryLabelHandler(Identifier identifier, Operation<String> original) {
             return Text.translatable("debug." + identifier.getNamespace() + "." + identifier.getPath()).getString();
@@ -23,13 +22,13 @@ public abstract class DebugOptionScreenMixin {
     }
 
     @Mixin(targets = "net/minecraft/client/gui/screen/DebugOptionsScreen$OptionsListWidget")
-    public static abstract class DebugOptionScreenOptionsListWidgetMixin {
+    abstract class DebugOptionScreenOptionsListWidgetMixin {
         @WrapOperation(method = "fillEntries", at = @At(value = "INVOKE", target = "Ljava/lang/String;contains(Ljava/lang/CharSequence;)Z"))
-        private boolean searchHandler(String string, CharSequence searchCharSequence, Operation<Boolean> original, @Local Map.Entry<Identifier, DebugHudEntry> entry) {
-            Identifier identifier = entry.getKey();
+        private boolean searchHandler(String string, CharSequence searchCharSequence, Operation<Boolean> original, @Local Map.Entry entry) {
+            Identifier identifier = (Identifier) entry.getKey();
             String searchStringLower = searchCharSequence.toString().toLowerCase();
-            return original.call(string.toLowerCase(), searchStringLower) ||
-                    original.call(Text.translatable("debug." + identifier.getNamespace() + "." + identifier.getPath()).getString().toLowerCase(), searchStringLower);
+            String translatedStringLower = Text.translatable("debug." + identifier.getNamespace() + "." + identifier.getPath()).getString().toLowerCase();
+            return original.call(string.toLowerCase(), searchStringLower) || original.call(translatedStringLower, searchStringLower);
         }
     }
 }
