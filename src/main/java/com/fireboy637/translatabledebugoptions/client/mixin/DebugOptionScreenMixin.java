e83d5d8687ceaea71eb40c3593d83222c7272324
HaveMC_XiaoCai <lxx601588@outlook.com>
2025-09-30 19:05:10 +0800

    update en_us.json and zh_cn.json to match Sodium and Iris

--- src/main/java/com/fireboy637/translatabledebugoptions/client/mixin/DebugOptionScreenMixin.java
+++ src/main/java/com/fireboy637/translatabledebugoptions/client/mixin/DebugOptionScreenMixin.java
@@ -3,6 +3,7 @@ package com.fireboy637.translatabledebugoptions.client.mixin;
 import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
 import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
 import com.llamalad7.mixinextras.sugar.Local;
+import net.minecraft.client.gui.hud.debug.DebugHudEntry;
 import net.minecraft.text.Text;
 import net.minecraft.util.Identifier;
 import org.spongepowered.asm.mixin.Mixin;
@@ -12,9 +13,10 @@ import java.util.Map;
 
 public interface DebugOptionScreenMixin {
     // Make getPath() return translated string. That's all.
-    // Though no code here uses Fabric API, but we still need to install it, or we can only see the translation keys.
+    // Though no code here uses Fabric API, we still need to install it, or we can only see the translation keys.
     @Mixin(targets = "net/minecraft/client/gui/screen/DebugOptionsScreen$Entry")
-    abstract class DebugOptionScreenEntryMixin {
+    abstract class EntryMixin {
+        // Need to use @WarpOperation instead of @Redirect, or it will be broken by Fabric API.
         @WrapOperation(method = "<init>", at = @At(value = "INVOKE", target = "Lnet/minecraft/util/Identifier;getPath()Ljava/lang/String;"))
         private String entryLabelHandler(Identifier identifier, Operation<String> original) {
             return Text.translatable("debug." + identifier.getNamespace() + "." + identifier.getPath()).getString();
@@ -22,13 +24,13 @@ public interface DebugOptionScreenMixin {
     }
 
     @Mixin(targets = "net/minecraft/client/gui/screen/DebugOptionsScreen$OptionsListWidget")
-    abstract class DebugOptionScreenOptionsListWidgetMixin {
+    abstract class OptionsListWidgetMixin {
         @WrapOperation(method = "fillEntries", at = @At(value = "INVOKE", target = "Ljava/lang/String;contains(Ljava/lang/CharSequence;)Z"))
-        private boolean searchHandler(String string, CharSequence searchCharSequence, Operation<Boolean> original, @Local Map.Entry entry) {
-            Identifier identifier = (Identifier) entry.getKey();
+        private boolean searchHandler(String originalString, CharSequence searchCharSequence, Operation<Boolean> original, @Local Map.Entry<Identifier, DebugHudEntry> entry) {
+            Identifier identifier = entry.getKey();
             String searchStringLower = searchCharSequence.toString().toLowerCase();
             String translatedStringLower = Text.translatable("debug." + identifier.getNamespace() + "." + identifier.getPath()).getString().toLowerCase();
-            return original.call(string.toLowerCase(), searchStringLower) || original.call(translatedStringLower, searchStringLower);
+            return original.call(originalString.toLowerCase(), searchStringLower) || original.call(translatedStringLower, searchStringLower);
         }
     }
 }
