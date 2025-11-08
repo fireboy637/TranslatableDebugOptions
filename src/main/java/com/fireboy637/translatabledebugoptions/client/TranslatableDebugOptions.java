cfc515384084b49c046dd823fa7b55be64d7b7e3
fireboy637 <lxx601588@outlook.com>
2025-11-08 12:23:06 +0800

    change to mojmap & remove junk code

--- src/main/java/com/fireboy637/translatabledebugoptions/client/TranslatableDebugOptions.java
+++ src/main/java/com/fireboy637/translatabledebugoptions/client/TranslatableDebugOptions.java
@@ -1,11 +1,17 @@
 package com.fireboy637.translatabledebugoptions.client;
 
+import net.minecraft.resources.ResourceLocation;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.util.ArrayList;
+import java.util.List;
 
-public interface TranslatableDebugOptions {
-    Logger LOGGER = LoggerFactory.getLogger("TranslatableDebugOptions");
-    ArrayList<String> missingTranslations = new ArrayList<>();
+public class TranslatableDebugOptions {
+    public static final Logger LOGGER = LoggerFactory.getLogger("TranslatableDebugOptions");
+    public static final List<String> missingTranslations = new ArrayList<>();
+
+    public static String getKey(ResourceLocation resourceLoc) {
+        return "debug." + resourceLoc.getNamespace() + "." + resourceLoc.getPath();
+    }
 }
--- src/main/java/com/fireboy637/translatabledebugoptions/client/mixin/DebugOptionScreenMixin.java
+++ src/main/java/com/fireboy637/translatabledebugoptions/client/mixin/DebugOptionScreenMixin.java
@@ -3,44 +3,50 @@ package com.fireboy637.translatabledebugoptions.client.mixin;
 import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
 import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
 import com.llamalad7.mixinextras.sugar.Local;
-import net.minecraft.client.gui.hud.debug.DebugHudEntry;
-import net.minecraft.text.Text;
-import net.minecraft.util.Identifier;
+import net.minecraft.client.gui.components.debug.DebugScreenEntry;
+import net.minecraft.network.chat.Component;
+import net.minecraft.resources.ResourceLocation;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import static com.fireboy637.translatabledebugoptions.client.TranslatableDebugOptions.LOGGER;
-import static com.fireboy637.translatabledebugoptions.client.TranslatableDebugOptions.missingTranslations;
 
 import java.util.Map;
 
-public interface DebugOptionScreenMixin {
+import static com.fireboy637.translatabledebugoptions.client.TranslatableDebugOptions.*;
+
+public class DebugOptionScreenMixin {
     // Make getPath() return translated string. That's all.
-    // Though no code here uses Fabric API, we still need to install it, or we can only see the translation keys.
-    @Mixin(targets = "net/minecraft/client/gui/screen/DebugOptionsScreen$Entry")
-    abstract class EntryMixin {
-        // Need to use @WarpOperation instead of @Redirect, or it will be broken by Fabric API.
-        @WrapOperation(method = "<init>", at = @At(value = "INVOKE", target = "Lnet/minecraft/util/Identifier;getPath()Ljava/lang/String;"))
-        private String entryLabelHandler(Identifier identifier, Operation<String> original) {
-            String key = "debug." + identifier.getNamespace() + "." + identifier.getPath();
-            Text translatedText = Text.translatable(key);
-            String translatedString = translatedText.getString();
-            if(translatedText.getString().contains(key) && !missingTranslations.contains(key)) {
-                LOGGER.warn("Missing translation for key: {}", key);
+    // Fabric API is required, as it loads the translations.
+
+    @Mixin(targets = "net/minecraft/client/gui/screens/debug/DebugOptionsScreen$OptionEntry")
+    public static class OptionEntryMixin {
+
+        // Need to use @WarpOperation instead of @Redirect and private method here, or it will be broken by Fabric API
+        // ... for some reason I don't know.
+        @WrapOperation(method = "<init>", at = @At(value = "INVOKE", target = "Lnet/minecraft/resources/ResourceLocation;getPath()Ljava/lang/String;"))
+        private String entryNameHandler(ResourceLocation instance, Operation<String> original) {
+            String key = getKey(instance);
+            String translatedStr = Component.translatable(key).getString();
+
+            // Log missing translations
+            if (translatedStr.equals(key) && !missingTranslations.contains(key)) {
+                LOGGER.warn("[TranslatableDebugOptions] Missing translation for key: {}", key);
                 // Prevent logging the same missing translation multiple times
                 missingTranslations.add(key);
             }
-            return translatedString;
+
+            return translatedStr;
         }
     }
 
-    @Mixin(targets = "net/minecraft/client/gui/screen/DebugOptionsScreen$OptionsListWidget")
-    abstract class OptionsListWidgetMixin {
-        @WrapOperation(method = "fillEntries", at = @At(value = "INVOKE", target = "Ljava/lang/String;contains(Ljava/lang/CharSequence;)Z"))
-        private boolean searchHandler(String originalString, CharSequence searchCharSequence, Operation<Boolean> original, @Local Map.Entry<Identifier, DebugHudEntry> entry) {
-            Identifier identifier = entry.getKey();
-            String searchStringLower = searchCharSequence.toString().toLowerCase();
-            String translatedStringLower = Text.translatable("debug." + identifier.getNamespace() + "." + identifier.getPath()).getString().toLowerCase();
-            return original.call(originalString.toLowerCase(), searchStringLower) || original.call(translatedStringLower, searchStringLower);
+    @Mixin(targets = "net/minecraft/client/gui/screens/debug/DebugOptionsScreen$OptionList")
+    public static class OptionsListMixin {
+        @WrapOperation(method = "updateSearch", at = @At(value = "INVOKE", target = "Ljava/lang/String;contains(Ljava/lang/CharSequence;)Z"))
+        private boolean searchHandler(String originalStr, CharSequence searchCharSeq, Operation<Boolean> original, @Local Map.Entry<ResourceLocation, DebugScreenEntry> entry) {
+            String searchStrLower = ((String) searchCharSeq).toLowerCase();
+            String translatedStrLower = Component.translatable(getKey(entry.getKey())).getString().toLowerCase();
+
+            return original.call(originalStr.toLowerCase(), searchStrLower) ||
+                    original.call(translatedStrLower, searchStrLower);
         }
     }
 }
